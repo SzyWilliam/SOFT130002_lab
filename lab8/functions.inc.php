4992e074db523ebc59045682eddac047856bcfea
William Song <szywilliam@foxmail.com>
2019-05-20 21:27:26 +0800

    lab-8 php
    php running env test and basic php

---
+++ lab8/functions.inc.php
@@ -0,0 +1,14 @@
+<?php
+
+    function outputOrderRow($file, $title, $quantity, $price) {
+        echo "<tr>";
+
+        echo "<td><img src = images/books/tinysquare/$file></td>";
+        echo "<td class=\"mdl-data-table__cell--non-numeric\">$title</td>";
+        echo "<td>$quantity</td>";
+        echo "<td>$$price</td>";
+        $total = $quantity * $price;
+        echo "<td>$$total.00</td>";
+        echo "</tr>";
+    }
+?>
